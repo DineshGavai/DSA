c266cbe9de8fcdd9b038e38f12cc9d683d92f873
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-02-01 21:07:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (33.33%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -2,22 +2,32 @@ class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
         if len(nums)==1:
             return True
-        parity=""
-        if nums[0]%2==0:
-            parity="even"
-        else:
-            parity="odd"
-        for i in range(1,len(nums)):
-            
-            if nums[i]%2==0 and parity=="even":
+        for i in range(len(nums)-1):
+            if ((nums[i]%2)==(nums[i+1]%2)):
                 return False
-            elif nums[i]%2==1 and parity=="odd":
-                return False
-            if nums[i]%2==0:
-                parity="even"
-            else:
-                parity="odd"
         return True
+        
+        
+        
+        
+        
+        
+        # parity=""
+        # if nums[0]%2==0:
+        #     parity="even"
+        # else:
+        #     parity="odd"
+        # for i in range(1,len(nums)):
+            
+        #     if nums[i]%2==0 and parity=="even":
+        #         return False
+        #     elif nums[i]%2==1 and parity=="odd":
+        #         return False
+        #     if nums[i]%2==0:
+        #         parity="even"
+        #     else:
+        #         parity="odd"
+        # return True
             
 
             
