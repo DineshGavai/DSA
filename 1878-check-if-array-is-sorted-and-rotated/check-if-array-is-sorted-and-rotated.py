34ed621a4ba1b686614bf780bd8b9c6c16a02193
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-02-02 08:54:45 +0530

    Time: 11 ms (3.15%) | Memory: 18.1 MB (13.49%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,19 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        def rotate(nums,k):
+            arr=nums.copy()
+            n=len(arr)
+            k=k%n
+            temp=arr[-k:]
+            arr[k:]=arr[:-k]
+            arr[:k]=temp
+            return arr
+        sorted_arr=sorted(nums)
+        if sorted_arr==nums:
+            return True
+        for i in range(1,len(nums)+1):
+            rotate_arr=rotate(nums,i)
+            print(rotate_arr)
+            if rotate_arr==sorted_arr:
+                return True
+        return False
