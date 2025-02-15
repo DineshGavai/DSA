3359d5007b39c8b4899e0fb5e3508a3961c976ba
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-02-15 23:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.81%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        seprated=sentence.split()
+        if len(seprated)==1:
+            if seprated[0][0]==seprated[0][-1]:
+                return True
+            else:
+                return False
+        nulla=0
+        first=seprated[0][0]
+        for i in range(1,len(seprated)):
+            if seprated[i-1][-1]==seprated[i][0]:
+                nulla+=1
+        if first==seprated[-1][-1]:
+            nulla+=1
+        return True if nulla==len(seprated) else False
