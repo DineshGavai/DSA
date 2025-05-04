f6a4f8c75d9d7d28c3cd7b2dbfe21e1ec9ac0d9e
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-05-04 21:55:32 +0530

    Time: 4 ms (11.07%) | Memory: 17.6 MB (98.84%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.py
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.py
@@ -1,9 +1,8 @@
 class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
         for i in range(len(nums)):
-            first_half=nums[:i+1]
-            second_half=nums[i:]
-            print("first Half:",first_half," second Half: ",second_half, "at index: ",i)
+            first_half=nums[:i]
+            second_half=nums[i+1:]
             if sum(first_half)==sum(second_half):
                 return i
         return -1
