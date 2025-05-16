df4091fbd70781723ed1fe0e1729bcb3a77b8e4b
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-05-16 10:23:16 +0530

    Time: 51 ms (58.58%) | Memory: 18 MB (56.18%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -2,14 +2,14 @@ class Solution:
     def partitionString(self, s: str) -> int:
         if len(set(s))==len(s):
             return 1
-        res=[]
+        res=1
         temp=""
         for i in range(len(s)):
             if s[i] not in temp:
                 temp+=s[i]
             else:
-                res.append(temp)
+                res+=1
                 temp=s[i]
 
 
-        return len(res)+1
+        return res
