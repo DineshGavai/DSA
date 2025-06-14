0571877f73fa92e27e884eaad13b8d845b5c7d83
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-06-14 11:04:06 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (11.69%) - LeetSync

--- 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.py
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.py
@@ -1,29 +1,26 @@
 class Solution:
     def minMaxDifference(self, num: int) -> int:
         to_max=''
+        to_min=''
         temp=list(str(num))
         for i in range(len(temp)):
-            if temp[i] != '9':
+            if temp[i] != '9' and to_max=='':
                 to_max=temp[i]
-                break
+            if temp[i] != '0'and to_min=='':
+                to_min=temp[i]
+                
         max_value=''
+        min_value=''
         for i in range(len(temp)):
             if temp[i]==to_max:
                 max_value+='9'
             else:
                 max_value+=temp[i]
-    
-        to_min=''
-        min_value=''
-        for i in range(len(temp)):
-            if temp[i] != '0':
-                to_min=temp[i]
-                break
-        for i in range(len(temp)):
             if temp[i]==to_min:
                 min_value+='0'
             else:
                 min_value+=temp[i]
+            
 
 
 
