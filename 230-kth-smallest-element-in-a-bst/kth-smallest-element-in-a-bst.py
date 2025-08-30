6ebcd1cf346bf4c462dc69af90e0b307094738f9
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-08-31 02:12:13 +0530

    Time: 1 ms (46.49%) | Memory: 21.2 MB (27.51%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -7,15 +7,12 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         arr=[]
-        curr=root
-        q=deque()
-        q.append(root)
-        while q:
-            node=q.popleft()
-            arr.append(node.val)
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-        arr.sort()
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            arr.append(root.val)
+            dfs(root.right)
+        dfs(root)
         return arr[k-1]
+      
