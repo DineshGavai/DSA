fd7d504fd50921af68274023aeed68003f0c826d
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-09-12 03:05:24 +0530

    Time: 59 ms (96.63%) | Memory: 20.7 MB (49.89%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -1,13 +1,25 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels=['a','e','i','o','u','A','E','I','O','U']
-        def is_vowel(s):
-            if s in vowels:
-                return True
-            return False
-        vowel=sorted([ i for i in s if is_vowel(i)])
-        s=list(s)
-        for i in range(len(s)):
-            if is_vowel(s[i]):
-                s[i]=vowel.pop(0)
-        return "".join(s)
+        vowels=set('aeiouAEIOU')
+        sorted_vowels=sorted([i for i in s if i in vowels])
+        res=[]
+        idx=0
+        for char in s:
+            if char in vowels:
+                res.append(sorted_vowels[idx])
+                idx+=1
+            else:
+                res.append(char)
+        return "".join(res)       
+
+        # vowels=['a','e','i','o','u','A','E','I','O','U']
+        # def is_vowel(s):
+        #     if s in vowels:
+        #         return True
+        #     return False
+        # vowel=sorted([ i for i in s if is_vowel(i)])
+        # s=list(s)
+        # for i in range(len(s)):
+        #     if is_vowel(s[i]):
+        #         s[i]=vowel.pop(0)
+        # return "".join(s)
