b9585ef56f47b58ac908c57f275bd297fc88ceb2
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-09-15 02:18:29 +0530

    Time: 3 ms (21.22%) | Memory: 17.9 MB (45.99%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,15 @@
+from functools import lru_cache
 class Solution:
-    def func(self,i,j,dp):
-        if i==0 and j==0:
+    @lru_cache
+    def func(self,i,j,m,n):
+        if i==m-1 and j==n-1:
             return 1
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        if i < 0 or j < 0:
+        if i >= m or j >= n:
             return 0
 
-        up=self.func(i-1,j,dp)
-        left=self.func(i,j-1,dp)
-        dp[i][j]=up+left
-        return dp[i][j]
+        down=self.func(i+1,j,m,n)
+        right=self.func(i,j+1,m,n)
+        return down+right
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[-1 for _ in range(n)]for _ in range(m)]
-        return self.func(m-1,n-1,dp)
+        return self.func(0,0,m,n)
        
