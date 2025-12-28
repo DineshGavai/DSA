0e09cf6d372344a3a8b2c8c282d543b05dc1bada
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2025-12-29 03:10:12 +0530

    Time: 6 ms (36.93%) | Memory: 17.3 MB (98.05%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,34 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        arr1=[]
-        arr2=[]
-        q1=deque([l1])
-        q2=deque([l2])
+        dummy = ListNode(0)
+        temp = dummy
+        carry = 0
         
-        while q1 or q2:
-            if q1:
-                node1=q1.popleft()
-                arr1.append(f"{node1.val}")
-                if node1.next:
-                    q1.append(node1.next)
-            if q2:
-                node2=q2.popleft()
-                arr2.append(f"{node2.val}")
-                if node2.next:
-                    q2.append(node2.next)
-
-        a=int("".join(reversed(arr1)))
-        b=int("".join(reversed(arr2)))
-        c=a+b
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            total = val1 + val2 + carry
+            carry = total // 10
+            
+            temp.next = ListNode(total % 10)
+            temp = temp.next
+            
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
         
-        arr = [int(digit) for digit in str(c)][::-1]
-        res=ListNode(arr[0])
-        current=res
-        for i in range(1,len(arr)):
-            current.next=ListNode(arr[i])
-            current=current.next
-        return res
-        
-
-        
+        return dummy.next
