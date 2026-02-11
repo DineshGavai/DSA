3485fc631ab780e28ee51b6991527a824c2f48de
Dinesh V. Gavai <111348314+DineshGavai@users.noreply.github.com>
2026-02-11 21:46:11 +0530

    Time: 11 ms (35.41%) | Memory: 19.7 MB (55.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,4 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned="".join(filter(str.isalnum,s)).lower()
-        return cleaned==cleaned[::-1]
+        string=""
+        for i in s:
+            if (i >= "a" and i <= "z") or (i >= "A" and i <= "Z") or  (i >= "0" and i <= "9"):
+                string+=i.lower()
+        i,j=0,len(string)-1
+        while j >= i:
+            if string[i]!=string[j]:
+                return False
+            i+=1
+            j-=1
+        return True
